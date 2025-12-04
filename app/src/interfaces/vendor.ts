8e4fc5446c633835fca907b714266c7c6cadfe06
jdhasko <dominik30000@gmail.com>
2025-12-04 09:25:29 +0100

    Vendor, MenuItem, Location, VendorResponse data models added

---
+++ app/src/interfaces/vendor.ts
@@ -0,0 +1,31 @@
+export interface Vendor {
+  id: string;
+  name: string;
+  cuisine: string;
+  city: string;
+  rating: number;
+  priceLevel: string;
+  thumbnail: string;
+  description: string;
+  location: Location;
+  menu: MenuItem[];
+  isFeatured: boolean;
+  isFavorite: boolean;
+}
+
+export interface MenuItem {
+  id: string;
+  name: string;
+  price: number;
+  spicy: boolean;
+  vegan: boolean;
+}
+
+export interface Location {
+  lat: number;
+  lng: number;
+}
+
+export interface VendorsResponse {
+  data: Vendor[];
+}
